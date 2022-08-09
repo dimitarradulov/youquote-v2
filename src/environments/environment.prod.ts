01ff0b8aaf0f420643ec46254f4f11e32c843cca
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-09 18:44:06 +0300

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'youquote-v2-7b922',
+    appId: '1:292802159230:web:fbea7babd2d9d76e1fee2f',
+    storageBucket: 'youquote-v2-7b922.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDistzZI_NwZCrK3AsscqoZty-PQIYifQk',
+    authDomain: 'youquote-v2-7b922.firebaseapp.com',
+    messagingSenderId: '292802159230',
+    measurementId: 'G-3SP0K06S0Q',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'youquote-v2-7b922',
+    appId: '1:292802159230:web:fbea7babd2d9d76e1fee2f',
+    storageBucket: 'youquote-v2-7b922.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDistzZI_NwZCrK3AsscqoZty-PQIYifQk',
+    authDomain: 'youquote-v2-7b922.firebaseapp.com',
+    messagingSenderId: '292802159230',
+    measurementId: 'G-3SP0K06S0Q',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
