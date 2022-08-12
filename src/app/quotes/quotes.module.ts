93eeadaf1764733802bbfa79d90e658a654b4cdd
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-12 15:55:51 +0300

    Declare single quote component

--- src/app/quotes/quotes.module.ts
+++ src/app/quotes/quotes.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuotesRoutingModule } from './quotes-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 import { AllQuotesComponent } from './all-quotes/all-quotes.component';
+import { SingleQuoteComponent } from './single-quote/single-quote.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [AllQuotesComponent],
-  imports: [CommonModule, QuotesRoutingModule],
+  declarations: [AllQuotesComponent, SingleQuoteComponent],
+  imports: [
+    CommonModule,
+    QuotesRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    SharedModule,
+  ],
 })
 export class QuotesModule {}
