3646a39cb123d17b3de6c617324851628e7ba85c
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-12 15:57:16 +0300

    Inject quotes and loading service + logic from getting quotes

--- src/app/quotes/all-quotes/all-quotes.component.ts
+++ src/app/quotes/all-quotes/all-quotes.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Quote } from '../../shared/models/quote.model';
+import { QuotesService } from '../quotes.service';
+import { LoadingService } from 'src/app/shared/loading/loading.service';
 
 @Component({
-  selector: 'app-all-quotes',
+  selector: 'all-quotes',
   templateUrl: './all-quotes.component.html',
-  styleUrls: ['./all-quotes.component.scss']
+  styleUrls: ['./all-quotes.component.scss'],
+  providers: [LoadingService],
 })
 export class AllQuotesComponent implements OnInit {
+  quotes$: Observable<Quote[]>;
 
-  constructor() { }
+  constructor(
+    private quotesService: QuotesService,
+    public loadingService: LoadingService
+  ) {}
 
   ngOnInit(): void {
+    const quotes$ = this.quotesService.getAll();
+    this.quotes$ = this.loadingService.showLoaderUntilComplete(quotes$);
   }
-
 }
