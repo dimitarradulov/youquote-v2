dd0449c98ae45ad49b2942d1e6b636ea7658bab0
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-14 13:45:29 +0300

    Add lazy loading to authenticate path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'quotes/all', pathMatch: 'full' },
+  {
+    path: 'authenticate',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
