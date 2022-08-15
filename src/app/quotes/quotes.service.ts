0fbe97da9fa66b854caec1f19fc8f2ab3962148d
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-15 17:13:18 +0300

    Add error handling method

--- src/app/quotes/quotes.service.ts
+++ src/app/quotes/quotes.service.ts
@@ -1,14 +1,39 @@
 import { Injectable } from '@angular/core';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import {
+  collection,
+  collectionData,
+  CollectionReference,
+  Firestore,
+} from '@angular/fire/firestore';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { ErrorMessageService } from '../shared/error-message/error-message.service';
 import { Quote } from '../shared/models/quote.model';
 
 @Injectable({ providedIn: 'root' })
 export class QuotesService {
-  constructor(private firestore: Firestore) {}
+  constructor(
+    private firestore: Firestore,
+    private errorMessageService: ErrorMessageService
+  ) {}
 
   getAll() {
-    const quotesCollection = collection(this.firestore, 'quotes');
-    return collectionData(quotesCollection) as Observable<Quote[]>;
+    const quotesCollection = collection(
+      this.firestore,
+      'quotes'
+    ) as CollectionReference<Quote>;
+
+    return collectionData(quotesCollection).pipe(
+      catchError((err) => this.handleError(err))
+    );
+  }
+
+  private handleError(err: Error) {
+    console.error(err);
+    const message =
+      'There was an error providing the quote/s.. Please try again!';
+    this.errorMessageService.showError(message);
+    return throwError(() => new Error(message));
   }
 }
