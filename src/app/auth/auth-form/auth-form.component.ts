3ecabda7eb440ab77407210a3914a462fdabde19
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-20 11:29:44 +0300

    Add auth form logic

--- src/app/auth/auth-form/auth-form.component.ts
+++ src/app/auth/auth-form/auth-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService, AuthFormValue } from './../auth.service';
 
 @Component({
   selector: 'auth-form',
@@ -8,10 +10,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AuthFormComponent implements OnInit {
   @Input() isSignUpForm = false;
+  @Output() formSubmit = new EventEmitter();
   signInForm: FormGroup;
   signUpForm: FormGroup = new FormGroup({});
+  error = null;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.signInForm = new FormGroup({
@@ -40,11 +44,21 @@ export class AuthFormComponent implements OnInit {
   }
 
   onSubmit() {
-    const currentForm = this.form;
+    const currentFormValue: AuthFormValue = this.form.value;
 
     if (!this.form.valid) return;
 
-    console.log(currentForm.value);
+    this.formSubmit.emit(currentFormValue);
+  }
+
+  async onSignInWithGoogle() {
+    try {
+      this.error = null;
+      await this.authService.signInWithGoogle();
+    } catch (err: any) {
+      console.error(err);
+      this.error = err.message;
+    }
   }
 
   passwordsMatch(control: FormControl) {
