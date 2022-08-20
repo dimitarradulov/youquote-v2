ce0785d437a3866ed4a6e8a7d69bd7f8f8195af8
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-20 11:31:05 +0300

    Add sign in logic

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService, AuthFormValue } from './../auth.service';
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async onSignIn(formValue: AuthFormValue) {
+    try {
+      await this.authService.signIn(formValue);
+    } catch (err) {
+      console.error(err);
+    }
   }
-
 }
