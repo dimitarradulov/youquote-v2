ad1a2e67333332d300b83b4ec81c431f5ba3ac99
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-20 11:31:17 +0300

    Add sign up logic

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthFormValue, AuthService } from './../auth.service';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async onSignUp(formValue: AuthFormValue) {
+    try {
+      await this.authService.signUp(formValue);
+    } catch (err) {
+      console.error(err);
+    }
   }
-
 }
