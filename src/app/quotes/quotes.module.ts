90abe6c9943232927d90cb1106200f4ef1c7c366
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-27 09:58:13 +0300

    Update quotes.module.ts

--- src/app/quotes/quotes.module.ts
+++ src/app/quotes/quotes.module.ts
@@ -6,12 +6,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AllQuotesComponent } from './all-quotes/all-quotes.component';
 import { SingleQuoteComponent } from './single-quote/single-quote.component';
 import { SharedModule } from '../shared/shared.module';
 import { MyQuotesComponent } from './my-quotes/my-quotes.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { EditQuoteComponent } from './edit-quote/edit-quote.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +22,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     SingleQuoteComponent,
     MyQuotesComponent,
     QuoteFormComponent,
+    EditQuoteComponent,
   ],
   imports: [
     CommonModule,
@@ -28,6 +32,8 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     MatButtonModule,
     MatDividerModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
     SharedModule,
   ],
 })
