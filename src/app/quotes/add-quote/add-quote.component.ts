b058fb956892a6d2bb5bfb47ebc2b5f5d5525a94
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-27 10:16:46 +0300

    Create add-quote component

---
+++ src/app/quotes/add-quote/add-quote.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-quote',
+  templateUrl: './add-quote.component.html',
+  styleUrls: ['./add-quote.component.scss'],
+})
+export class AddQuoteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
