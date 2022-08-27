26aa7058fa6cc9b4db19e613b33599afcf820938
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-27 09:58:11 +0300

    Update quotes-routing.module.ts

--- src/app/quotes/quotes-routing.module.ts
+++ src/app/quotes/quotes-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AllQuotesComponent } from './all-quotes/all-quotes.component';
 import { MyQuotesComponent } from './my-quotes/my-quotes.component';
+import { EditQuoteComponent } from './edit-quote/edit-quote.component';
 
 const routes: Routes = [
   { path: 'all', component: AllQuotesComponent },
   { path: 'my', component: MyQuotesComponent },
+  { path: 'edit/:quoteId', component: EditQuoteComponent },
 ];
 
 @NgModule({
