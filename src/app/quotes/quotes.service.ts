fac620bde6090b420de9880dcf4b4517f375887b
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-08-27 09:58:15 +0300

    Update quotes.service.ts

--- src/app/quotes/quotes.service.ts
+++ src/app/quotes/quotes.service.ts
@@ -7,8 +7,13 @@ import {
   where,
   query,
   getDocs,
+  doc,
+  docSnapshots,
+  DocumentReference,
+  updateDoc,
 } from '@angular/fire/firestore';
-import { first, lastValueFrom } from 'rxjs';
+import { first, lastValueFrom, from, throwError } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 import { Quote } from '../shared/models/quote.model';
 import { AuthService } from '../auth/auth.service';
@@ -17,32 +22,58 @@ import { AuthService } from '../auth/auth.service';
 export class QuotesService {
   constructor(private firestore: Firestore, private authService: AuthService) {}
 
-  get quotesCollection() {
-    return collection(this.firestore, 'quotes') as CollectionReference<Quote>;
-  }
-
   getAll() {
-    return collectionData(this.quotesCollection, { idField: 'id' });
+    return collectionData(this.quotesCollection, { idField: 'id' }).pipe(
+      shareReplay()
+    );
   }
 
   async getAllUserQuotes() {
-    const quotes: Quote[] = [];
+    try {
+      const quotes: Quote[] = [];
 
-    const user = await lastValueFrom(this.authService.user$.pipe(first()));
+      const user = await lastValueFrom(this.authService.user$.pipe(first()));
 
-    const q = query(this.quotesCollection, where('creator', '==', user?.uid));
+      const q = query(this.quotesCollection, where('creator', '==', user?.uid));
 
-    const userQuotes = await getDocs(q);
-    userQuotes.forEach((doc) => {
-      quotes.push({
-        id: doc.id,
-        author: doc.data().author,
-        authorImageUrl: doc.data().authorImageUrl,
-        content: doc.data().content,
-        createdAt: doc.data().createdAt,
+      const userQuotes = await getDocs(q);
+      userQuotes.forEach((doc) => {
+        quotes.push({
+          id: doc.id,
+          author: doc.data().author,
+          authorImageUrl: doc.data().authorImageUrl,
+          content: doc.data().content,
+          createdAt: doc.data().createdAt,
+        });
       });
-    });
 
-    return quotes;
+      return quotes;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+
+  getOne(quoteId: string) {
+    const document = this.getQuoteDocument(quoteId);
+
+    return docSnapshots(document).pipe(
+      map((docSnapshot) => <Quote>docSnapshot.data()),
+      shareReplay()
+    );
+  }
+
+  update(quoteId: string, changes: Partial<Quote>) {
+    const document = this.getQuoteDocument(quoteId);
+
+    return from(updateDoc(document, changes));
+  }
+
+  private get quotesCollection() {
+    return collection(this.firestore, 'quotes') as CollectionReference<Quote>;
+  }
+
+  private getQuoteDocument(quoteId: string) {
+    return doc(this.firestore, `quotes/${quoteId}`) as DocumentReference<Quote>;
   }
 }
