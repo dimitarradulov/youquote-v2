731cffeda55802f2dc90a82d20f5cea2c283fc3e
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-09-03 22:43:11 +0300

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 import { LoadingComponent } from './loading/loading.component';
 import { ErrorMessageComponent } from './error-message/error-message.component';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
-  declarations: [LoadingComponent, ErrorMessageComponent],
+  declarations: [LoadingComponent, ErrorMessageComponent, DialogComponent],
   imports: [
     CommonModule,
     MatProgressSpinnerModule,
     MatCardModule,
     MatIconModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
   exports: [LoadingComponent, ErrorMessageComponent],
 })
