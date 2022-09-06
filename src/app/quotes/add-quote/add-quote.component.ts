201fe150c677e793e3c79263d00c075ee53406f4
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-09-06 10:08:28 +0300

    Add adding quote method

--- src/app/quotes/add-quote/add-quote.component.ts
+++ src/app/quotes/add-quote/add-quote.component.ts
@@ -1,12 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Quote } from 'src/app/shared/models/quote.model';
+import { QuotesService } from '../quotes.service';
+import { ErrorMessageService } from 'src/app/shared/error-message/error-message.service';
+import { LoadingService } from 'src/app/shared/loading/loading.service';
 
 @Component({
   selector: 'add-quote',
   templateUrl: './add-quote.component.html',
   styleUrls: ['./add-quote.component.scss'],
+  providers: [LoadingService, ErrorMessageService],
 })
 export class AddQuoteComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private quotesService: QuotesService,
+    private router: Router,
+    public errorMessageService: ErrorMessageService,
+    public loadingService: LoadingService
+  ) {}
 
   ngOnInit(): void {}
+
+  onAdd(quoteData: Partial<Quote>) {
+    const addQuote$ = this.quotesService.add(quoteData).pipe(
+      catchError((err) => {
+        console.error(err);
+        this.errorMessageService.showError(err.message);
+        return throwError(() => new Error(err));
+      })
+    );
+
+    this.loadingService.showLoaderUntilComplete(addQuote$).subscribe({
+      next: () => this.router.navigate(['/']),
+    });
+  }
 }
